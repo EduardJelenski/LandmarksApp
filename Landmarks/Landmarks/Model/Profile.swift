4166e6235b608e16947efff64b8c5645656ef3a6
Еленский Эдуард Владиславович <eduardelenskii@gmail.com>
2022-08-14 16:59:58 +0300

    Working with UI Controls

---
+++ Landmarks/Landmarks/Model/Profile.swift
@@ -0,0 +1,27 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by Еленский Эдуард on 14.08.2022.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+    
+    static let `default` = Profile(username: "g_kumar")
+    
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+    
+        var id: String { rawValue }
+    }
+    
+}
